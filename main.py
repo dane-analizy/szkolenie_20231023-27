a1ecc46d1ad18e68f55586d999c0a57504a6c2f8
Łukasz Prokulski <prokulski@gmail.com>
2023-10-23 10:16:22 +0200

    szkolenie

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello world!")
+# print("Hello world!")
+
+# zmienna = 123
+# zmienna2 = "abc"
+
+# print(zmienna)
+# print("zmienna")
+# print(zmienna2)
+
+# zmienna = zmienna + 5
+# print(zmienna)
+# print(zmienna + 10)
+# print(zmienna)
+
+# definicja zmiennych
+tekst_a = "aaaa"
+tekst_b = "bbbb"
+liczba = 123
+
+# dodanie zmiennych tekstowych - "sklejenie ciągów"
+tekst_wynikowy = tekst_a + tekst_b
+print(tekst_wynikowy)
+
+# dodanie liczby do tekstu
+zmienna_wynikowa = tekst_a + str(liczba)
+print(zmienna_wynikowa)
