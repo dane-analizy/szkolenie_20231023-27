00c9efb3c399811c8df6816f6174e475e2f14d29
Łukasz Prokulski <prokulski@gmail.com>
2023-10-23 10:23:50 +0200

    szkolenie

--- main.py
+++ main.py
@@ -13,14 +13,29 @@
 # print(zmienna)
 
 # definicja zmiennych
-tekst_a = "aaaa"
-tekst_b = "bbbb"
-liczba = 123
+# tekst_a = "aaaa"
+# tekst_b = "bbbb"
+# liczba = 123
 
-# dodanie zmiennych tekstowych - "sklejenie ciągów"
-tekst_wynikowy = tekst_a + tekst_b
-print(tekst_wynikowy)
+# # dodanie zmiennych tekstowych - "sklejenie ciągów"
+# tekst_wynikowy = tekst_a + tekst_b
+# print(tekst_wynikowy)
+#
+# # dodanie liczby do tekstu
+# zmienna_wynikowa = tekst_a + str(liczba)
+# print(zmienna_wynikowa)
+#
+# # formatowanie printa
+# print("Tekst: {}\nLiczba: {}".format(tekst_a, liczba))
+#
+# # za dużo zmiennych, za mało miejsca na nie
+# print("Tekst: {}\nLiczba: {}".format(tekst_a, liczba, tekst_b))
 
-# dodanie liczby do tekstu
-zmienna_wynikowa = tekst_a + str(liczba)
-print(zmienna_wynikowa)
+# za mało zmiennych
+# print("Tekst: {}\nLiczba: {}".format(tekst_a))
+
+# f-string
+# print(f"Tekst : {tekst_a}\nLiczba: {liczba}")
+
+# f-string z wyrażeniem
+# print(f"Tekst : {tekst_a}\nLiczba: {liczba+10}")
