02a5e0ba426ddf8fd8475562e3a57031cf0086ae
Łukasz Prokulski <prokulski@gmail.com>
2023-10-24 10:01:00 +0200

    dzień drugi

--- main.py
+++ main.py
@@ -49,3 +49,62 @@
 
 #### ZADANIE 17
 # Wypisz na ekranie tabliczkę mnożenia w zakresie 1-100, pokazując działanie i jego wynik
+
+# for x in range(10):
+#     for y in range(10):
+#         print(f"{x+1} * {y+1} = {(x+1)*(y+1)}")
+
+# print("iterator")
+# lista = range(20)
+# print(lista)
+# print(lista[::5])
+#
+# print("lista z iteratora")
+# lista_lista = list(lista)
+# print(lista_lista)
+# print(lista_lista[::5])
+
+
+# lista = [100, 101, 102, 1]
+#
+# # dodawanie elementów do listy
+# print(lista)
+# lista.append(1)
+# print(lista)
+# lista.append(2)
+# lista.append(1)
+# print(lista)
+#
+# # wstawianie w środek listy
+# lista.insert(0, 3)
+# print(lista)
+# lista.insert(2, 10)
+# print(lista)
+#
+# # usunięcie pierwszej napotkanej wartości
+# lista.remove(1)
+# print(lista)
+#
+# # usunięcie elementu o konkretnym indeksie
+# lista.pop(4)
+# print(lista)
+
+# zamiana elementu na liście
+# lista = list(range(1, 11))
+# print(lista)
+# lista[5] = "a"
+# print(lista)
+
+# "pseudo" kopia listy - zmiana na oryginale zmienia "kopię"
+# lista = list(range(1, 11))
+# lista_zapasowa = lista
+# print(lista_zapasowa)
+# lista[5] = 'abc'
+# print(lista_zapasowa)
+#
+# # "prawdziwa" kopia listy - zmiana na oryginale NI Ezmienia kopii
+# lista = list(range(1, 11))
+# lista_zapasowa = lista.copy()
+# print(lista_zapasowa)
+# lista[5] = 'abc'
+# print(lista_zapasowa)
