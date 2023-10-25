4a74ef1d2220aaa0a4552e556df8a619d1e7f89d
Łukasz Prokulski <prokulski@gmail.com>
2023-10-25 09:12:29 +0200

    dzien trzeci

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-zmienna = "linia 2"
-with open("dane_oczyszczone.csv", 'w') as f:
-    f.write("linia 1\n")
-    f.write(f"{zmienna}\n")
 
+# zapisywanie tekstu do pliku
 
+# context manager - konstrukcja with ...:
+# zmienna = "linia 2"
+# with open("dane_oczyszczone.csv", 'w') as f:
+#     f.write("linia 1\n")
+#     f.write(f"{zmienna}\n")
+
+# bez context managera - otwarcie pliku, operacje, zamknięcie
 # f = open(...)
-# # kolejne instruncje
+# # kolejne instrukcje
 # f.close()
 
 #### ZADANIE 30
