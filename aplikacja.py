5d2efb42a5d9b01f743cf4af88e2f47abc2cdff5
Łukasz Prokulski <prokulski@gmail.com>
2023-10-27 13:34:30 +0200

    dzień piąty

--- aplikacja.py
+++ aplikacja.py
@@ -4,5 +4,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def home_page():
+    return "Moja Strona główna"
+
+
+@app.route('/test')
+def test_page():
+    return "strona testowa"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
