b89c050599acf1c970aab466256f4cac0fd22d33
Łukasz Prokulski <prokulski@gmail.com>
2023-10-27 11:35:55 +0200

    dzień piąty

---
+++ dowcipy.py
@@ -0,0 +1,5 @@
+# https://official-joke-api.appspot.com/random_joke
+# {"type":"general","setup":"What do you call a girl between two posts?","punchline":"Annette.","id":210}
+
+### ZADANIE 54
+# Pobierz i wyświetl na ekranie dowcip z API https://official-joke-api.appspot.com/random_joke
