6720d3f3c9c105a8cb02d75b5a9c20fb21671ef8
Łukasz Prokulski <prokulski@gmail.com>
2023-10-27 14:52:42 +0200

    dzień piąty

--- aplikacja.py
+++ aplikacja.py
@@ -2,8 +2,12 @@
 
 from flask import Flask, render_template
 from tools.file_op import get_file_content
+from tools.db_op import get_sql_results
+from tools.config import load_config
 
 app = Flask(__name__)
+config = load_config()
+
 
 @app.route('/')
 def home_page():
@@ -44,6 +48,13 @@ def list_page():
 
     return render_template('list_page.html', users=dane)
 
+@app.route('/cars')
+def car_lista():
+    dane = get_sql_results(config, "SELECT * FROM cars;")
+    dane = [ {"model":el[0],
+            "marka":el[1]}
+             for el in dane ]
+    return render_template('cars_page.html', cars=dane)
 
 if __name__ == "__main__":
     app.run(debug=True)
@@ -57,7 +68,6 @@ if __name__ == "__main__":
 
 #### ZADANIE 56
 # Przygotuj template, który wyświetli dane ze słownika opisującego samochód.
-
 
 #### ZADANIE 57
 # Przygotuj template wyświeltający listę danych pobranych z bazy danych - np. listę samochodów
