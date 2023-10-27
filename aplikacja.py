cc9f424380fd7061875aacaf24b515328294f42e
Łukasz Prokulski <prokulski@gmail.com>
2023-10-27 13:39:41 +0200

    dzień piąty

--- aplikacja.py
+++ aplikacja.py
@@ -13,6 +13,14 @@ def home_page():
 def test_page():
     return "strona testowa"
 
+@app.route('/shop')
+def shop_page():
+    return "Shop"
+
+@app.route('/contact')
+def contact_page():
+    return "Contact info: ....."
+
 
 if __name__ == "__main__":
     app.run(debug=True)
