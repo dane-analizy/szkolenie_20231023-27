f96d4697508541914bbf4e27ad66314b32610755
Łukasz Prokulski <prokulski@gmail.com>
2023-10-27 09:18:33 +0200

    dzień piąty

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import yaml
 with open("db_config.yaml", "r", encoding="utf-8") as f:
     config = yaml.safe_load(f)
 
-print(config)
-
 # pakiet do łączenia się z postgresql
 # pip install psycopg2
 
@@ -16,3 +14,7 @@ print(config)
 
 # SQLAlchemy - pakiet do łączenia się do dowolnej bazy
 # pip install sqlalchemy
+
+# connection string
+conn_str = f"postgresql+psycopg2://{config['db_user']}:{config['db_pass']}@{config['db_host']}:{config['db_port']}/{config['db_name']}"
+print(conn_str)
