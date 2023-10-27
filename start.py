5f7d899fb73a4f7a8a3092a242cd09ba34bd3e45
Łukasz Prokulski <prokulski@gmail.com>
2023-10-27 11:22:46 +0200

    dzień piąty

--- start.py
+++ start.py
@@ -1,14 +1,27 @@
 from tools.config import load_config
 from tools.db_op import get_sql_results
+from tools.file_op import save_to_file
 
+LISTA_MAREK = ['skoda', 'ford']
 
-# print("jestem na początku")
 
 def main():
     config = load_config()
-    results = get_sql_results(config, "SELECT * FROM cars where lower(marka) = 'skoda';")
-    print(results)
+    for marka in LISTA_MAREK:
+        results = get_sql_results(config,
+                                  f"SELECT * FROM cars WHERE lower(marka) = '{marka}';")
+        save_to_file(results, f"auta_{marka}.csv")
 
 
 if __name__ == "__main__":
     main()
+
+
+# select imie, nazwisko, numer_konta from klienci
+
+"""
+for klient in results:
+    f"select * from rachunki where numer_konta = '{klient['numer_konta']}"
+
+    select * from rachunki where numer_konta in (select numer_konta from klienci)
+"""
