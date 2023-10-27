338d1c1cde4afb97fb7dcbc6f85443bf5d465d44
Łukasz Prokulski <prokulski@gmail.com>
2023-10-27 13:55:28 +0200

    dzień piąty

--- aplikacja.py
+++ aplikacja.py
@@ -9,6 +9,12 @@ def home_page():
     return render_template('home_page.html')
 
 
+@app.route('/test')
+def test_page():
+    return render_template('test_page.html')
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
@@ -16,5 +22,5 @@ if __name__ == "__main__":
 #### ZADANIE 54
 # Przygotuj w aplikacji Flask 3 podstrony
 
-#### ZADANIE 54
+#### ZADANIE 55
 # Przygotuj dodatkowy template dla aplikacji Flask
