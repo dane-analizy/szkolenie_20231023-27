f146018bd17e6c71f64626df846ac3d18ad8f0f3
Łukasz Prokulski <prokulski@gmail.com>
2023-10-27 10:18:37 +0200

    dzień piąty

--- tools/config.py
+++ tools/config.py
@@ -1,4 +1,6 @@
 import yaml
 
-with open("db_config.yaml", "r", encoding="utf-8") as f:
-    config = yaml.safe_load(f)
+def load_config(config_file = "db_config.yaml"):
+    with open(config_file, "r", encoding="utf-8") as f:
+        config = yaml.safe_load(f)
+    return config
