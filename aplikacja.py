f8f82a6cd68a92c318542f3e9f18d49c4dc954ec
Łukasz Prokulski <prokulski@gmail.com>
2023-10-27 13:44:46 +0200

    dzień piąty

--- aplikacja.py
+++ aplikacja.py
@@ -1,31 +1,12 @@
 # pip install Flask
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return """
-    <html>
-    <head>
-        <title>Moja strona</title>
-    </head>
-    <body>
-        <h1>Witam na mojej stronie!</h1>
-        <table border="1">
-        <tr>
-            <td>wiersz 1, kolumna 1</td>
-            <td>wiersz 1, kolumna 2</td>
-        </tr>
-        <tr>
-            <td>wiersz 2, kolumna 1</td>
-            <td>wiersz 2, kolumna 2</td>
-        </tr>
-        </table>
-    </body>
-    </html>
-    """
+    return render_template('templates/home_page.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
